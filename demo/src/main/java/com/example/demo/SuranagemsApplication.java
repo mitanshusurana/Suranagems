f8b89bc5b700b00bd741027209824f362c3903cc
mitanshusurana <55337570+mitanshusurana@users.noreply.github.com>
2023-12-23 21:27:52 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/SuranagemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuranagemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuranagemsApplication.class, args);
+	}
+
+}
